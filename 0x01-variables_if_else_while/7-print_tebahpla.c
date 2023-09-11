d80444819d5dcb42ea3c7c7fed7d11fc2cefd143
melendra <khanijoymvirtualassistant@gmail.com>
2023-09-11 03:00:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n <= 97)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
