b00aeeca7518a05ec47a8f6ef69535ca4702e157
melendra <khanijoymvirtualassistant@gmail.com>
2023-09-20 15:07:45 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -23,27 +23,3 @@ char *_strncpy(char *dest, char *src, int n)
         }
         return (dest);
 }
-====================================
-3-strcmp.c
-#include "main.h"
-/**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- *
- * Return: s1[i] - s2[i]
- */
-int _strcmp(char *s1, char *s2)
-{
-        int i;
-        i = 0;
-        while (s1[i] != '\0' && s2[i] != '\0')
-        {
-                if (s1[i] != s2[i])
-                {
-                        return (s1[i] - s2[i]);
-                }
-                i++;
-        }
-        return (0);
-}
