71eb935e907ed565daf69cb7ebfaeff601c616ac
melendra <khanijoymvirtualassistant@gmail.com>
2023-09-21 01:38:54 -0700

    numeracy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,18 @@
  */
 void reverse_array(int *a, int n)
 {
-        int i;
-        int t;
-        for (i = 0; i < n--; i++)
-        {
-                t = a[i];
-                a[i] = a[n];
-                a[n] = t;
-        }
+	int i;
+
+	int t;
+
+	for (i = 0; i < n--; i++)
+
+	{
+		t = a[i];
+
+		a[i] = a[n];
+
+		a[n] = t;
+
+	}
 }
