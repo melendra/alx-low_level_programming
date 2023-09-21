680e37e31b1a98219df5bc6111085307a0ad7e4a
melendra <khanijoymvirtualassistant@gmail.com>
2023-09-21 01:50:41 -0700

    look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,19 @@
  */
 char *string_toupper(char *n)
 {
-        int i;
-        i = 0;
-        while (n[i] != '\0')
-        {
-                if (n[i] >= 'a' && n[i] <= 'z')
-                        n[i] = n[i] - 32;
-                i++;
-        }
-        return (n);
+	int i;
+
+	i = 0;
+
+	while (n[i] != '\0')
+
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+
+			n[i] = n[i] - 32;
+		i++;
+
+	}
+
+	return (n);
 }
