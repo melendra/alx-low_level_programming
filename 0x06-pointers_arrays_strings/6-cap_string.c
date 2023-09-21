9d46ebce58bd3e43be52fc515a19654932eba688
melendra <khanijoymvirtualassistant@gmail.com>
2023-09-21 05:31:19 -0700

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,40 +9,42 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while index = 0;
+	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index} <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+
 		if (str[index - 1] == ' ' ||
 
-				str[index - 1] == '\t' ||
+		    str[index - 1] == '\t' ||
 
-				str[index - 1] == '\n' ||
+		    str[index - 1] == '\n' ||
 
-				str[index - 1] == ',' ||
+		    str[index - 1] == ',' ||
 
-				str[index - 1] == ';' ||
+		    str[index - 1] == ';' ||
 
-				str[index - 1] == '.' ||
+		    str[index - 1] == '.' ||
 
-				str[index - 1] == '!' ||
+		    str[index - 1] == '!' ||
 
-				str[index - 1] == '?' ||
+		    str[index - 1] == '?' ||
 
-				str[index - 1] == '"' ||
+		    str[index - 1] == '"' ||
 
-				str[index - 1] == '(' ||
+		    str[index - 1] == '(' ||
 
-				str[index - 1] == ')' ||
+		    str[index - 1] == ')' ||
 
-				str[index - 1] == '{' ||
+		    str[index - 1] == '{' ||
 
-				str[index - 1] == '}' ||
+		    str[index - 1] == '}' ||
 
-				index == 0)
+		    index == 0)
 
-				str[index] -= 32;
-}
+		    str[index] -= 32;
 
-return (str);
+		index++;
+	}
+	return (str);
 }
