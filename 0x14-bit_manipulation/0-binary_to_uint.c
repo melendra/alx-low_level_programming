ac0677640302b4184b47ed11eeda055357ab2391
melendra <khanijoymvirtualassistant@gmail.com>
2023-10-26 14:05:07 -0700

    babyblue

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int alx;
+	int blue;
 
-	unsigned int abraham = 0;
+	unsigned int baby = 0;
 
 	if (!b)
 		return (0);
 
-	for (alx = 0; b[alx]; alx++)
+	for (blue= 0; b[blue]; blue++)
 
 	{
-		if (b[alx] < '0' || b[alx] > '1')
+		if (b[blue] < '0' || b[blue] > '1')
 
 			return (0);
 
-		abraham = 2 * abraham + (b[alx] - '0');
+		baby = 2 * baby + (b[blue] - '0');
 	}
-	return (abraham);
+	return (baby);
 }
